bb51f66573374a4abcc7dccb8c6d8f92fee268c0
Felipe Muñoz <analista.felipe.m@gmail.com>
2023-05-26 18:01:21 -0400

    Se completa la logica del desafio.

---
+++ assets/js/main.js
@@ -0,0 +1,120 @@
+/*** Variables. ***/
+const btnCalcular = document.querySelector('#btnCalcular');
+const btnGasto = document.querySelector('#btnGasto');
+const totalPresupuesto = document.querySelector('#totalPresupuesto');
+const totalGasto = document.querySelector('#totalGasto');
+const totalSaldo = document.querySelector('#totalSaldo');
+/* const gastoNombre = document.querySelector('#gastoNombre'); */
+/* const gastoValor = document.querySelector('#gastoValor'); */
+/* const gastoEliminar = document.querySelector('#gastoEliminar'); */
+let arregloEntradas = [];
+let arregloSalidas = [];
+
+/*** Eventos ***/
+btnCalcular.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const inputIngresoPresupuesto = document.querySelector('#inputIngresoPresupuesto').valueAsNumber;
+
+    if (isNaN(inputIngresoPresupuesto)) {
+        alert('Debe agregar un presupuesto inicial antes.')
+    } else if (inputIngresoPresupuesto < 0 ) {
+        alert('El presupuesto no puede ser un numero negativo')
+    } else {
+        arregloEntradas.push(inputIngresoPresupuesto);
+
+        agregarPresupuesto();
+        limpiarInputPresupuesto();
+    }
+});
+
+btnGasto.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const inputGastoNombre = document.querySelector('#inputGastoNombre').value;
+    const inputGastoValor = document.querySelector('#inputGastoValor').valueAsNumber;
+
+    if (inputGastoNombre == "") {
+        alert('Debe ingresar el nombre del gasto.')
+    } else if (isNaN(inputGastoValor)) {
+        alert('Debe ingresar el valor del gasto.')
+    } else if (inputGastoValor < 0 ) {
+        alert('El valor no puede ser un numero negativo.')
+    } else {
+        let objeto = {
+            id: arregloSalidas.length + 1,
+            nombre: inputGastoNombre,
+            valor: inputGastoValor,
+        }
+
+        arregloSalidas.push(objeto);
+
+        agregarGasto()
+        limpiarInputGasto()
+    }
+});
+
+/*** Funciones ***/
+function agregarPresupuesto() {
+    totalPresupuesto.innerHTML = "";
+
+    let total = arregloEntradas.reduce(function (total, presupuesto) {
+        return total + presupuesto;
+    }, 0);
+
+    totalPresupuesto.innerHTML = Number(total).toLocaleString("es-CL");
+
+    calcularSaldoFinal()
+}
+
+function agregarGasto() {
+
+    totalGasto.innerHTML = "";
+
+    let total = arregloSalidas.reduce(function (total, gasto) {
+        return total + gasto.valor;
+    }, 0);
+
+    const gastoResultado = document.querySelector('#gastoResultado');
+    gastoResultado.innerHTML = "";
+
+    arregloSalidas.forEach(gasto => {
+        const valorFormateado = Number(gasto.valor).toLocaleString("es-CL");
+        gastoResultado.innerHTML += `
+        <div class="detail-spent--row">
+            <p id="gastoNombre">${gasto.nombre}</p>
+            <p id="gastoValor">$ ${valorFormateado}</p>
+            <p id="gastoEliminar" onclick="quitarGasto(${gasto.id})"><i class="fa-solid fa-trash-can"></i></p>
+        </div>
+        <hr class="detail-spent--line2">
+        `;
+    });
+
+    totalGasto.innerHTML = Number(total).toLocaleString("es-CL");
+    calcularSaldoFinal()
+}
+
+function quitarGasto(id) {
+    let quitar = arregloSalidas.findIndex(gasto => gasto.id == id);
+    arregloSalidas.splice(quitar, 1);
+
+    agregarGasto()
+    calcularSaldoFinal()
+}
+
+function calcularSaldoFinal() {
+    let labelPresupesto = totalPresupuesto.innerHTML.replace(/[.]/g, '');
+    let labelGasto = totalGasto.innerHTML.replace(/[.]/g, '');
+
+    let labelSaldo = labelPresupesto - labelGasto
+    totalSaldo.innerHTML = Number(labelSaldo).toLocaleString("es-CL");
+}
+
+function limpiarInputPresupuesto(){
+    document.querySelector('#inputIngresoPresupuesto').value = "";
+}
+
+function limpiarInputGasto(){
+    document.querySelector('#inputGastoNombre').value = "";
+    document.querySelector('#inputGastoValor').value = "";
+}
